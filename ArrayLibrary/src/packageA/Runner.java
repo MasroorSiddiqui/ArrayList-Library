446db01c78dfc4d0f311351539d2dcc97424917a
MasroorSiddiqui <112581987+MasroorSiddiqui@users.noreply.github.com>
2023-02-06 22:43:52 -0800

    Update Runner.java

--- ArrayLibrary/src/packageA/Runner.java
+++ ArrayLibrary/src/packageA/Runner.java
@@ -4,76 +4,101 @@ import java.util.Scanner;
 
 
 public class Runner {
+	//Declaring ArrayList to be used across different methods
 	private static ArrayList<Book> bookList = new ArrayList<>();
-	private static String[] subjectArray = new String[]{"Programming", "Data Structures", "Algorithms", "Operating Systems", "Gaming"};
-	private static int[] subjectCount = new int[]{0,0,0,0,0};
 	private static ArrayList<Integer> yearCount = new ArrayList<>();
 	
+	//Main method
 	public static void main(String[] args)
 	{
+		//Creates book objects and puts them in BookList
 		setYears();
-		for (int i = 0; i <= 20; i++) {
+		for (int i = 0; i <= 20; i++) 
 			bookList.add(new Book("Book " + i, "Math", 0, 0, 0));	
-		}
-		for (Book book: bookList) {
+		
+		//Assigns each book in bookList a random page number, review rating, subject and year of publication
+		for (Book book: bookList) 
+		{
 			randomTotalPages(book);
 			randomReviewRating(book);
-			randomSubject(book);
+			randomSubject();
 			randomYear(book);
 		}
+		
+		//Creates menu for user
 		createMenu();
 	}
+	
+	//Sets each book in bookList to a random number between 50 to 100
 	public static void randomTotalPages(Book b)
 	{
 		int number = (int)(Math.random()*50) + 50;
 		b.setTotalPages(number);
 	}
+	
+	//Sets each book in bookList to a random rating between 0.1 to 10.0 
 	public static void randomReviewRating(Book b)
 	{
 		double number = (Math.random()*9.9) + 0.1;
 		b.setReviewRating(number);
 	}
-	public static void randomSubject(Book b)
+	
+	//Sets each book in bookList to a random subject
+	public static void randomSubject()
 	{
-		boolean check = true;
-	    while(check == true)
+		//Set of subjects each book can have
+		ArrayList <String> subjectArray = new ArrayList<>();
+		for (int i = 0; i < 5; i++)
+		{
+			subjectArray.add("Programming");
+			subjectArray.add("Gaming");
+			subjectArray.add("Algorithms");
+			subjectArray.add("Operating Systems");
+			subjectArray.add("Data Structures");
+		}
+
+		//For each book in bookList assign it a subject from subjectArray, and then remove it from subjectArray
+		for (Book b: bookList)
 	    {
-	    	int counter = 0;
-	        for(int i: subjectCount){
-	            if(i > 5)
-	              counter++;
-	          }
-	        int number = (int)(Math.random()*subjectArray.length);
-	        if (counter == subjectCount.length)
-	          break;
-	        else{
-	          if(subjectCount[number] < 5){
-	           b.setSubject(subjectArray[number]);
-	           subjectCount[number] += 1;
-	           check = false;
-	          }
-	        }
+	    	int number = (int)(Math.random()*subjectArray.size());
+	    	b.setSubject(subjectArray.get(number));
+	    	subjectArray.remove(number);
 	    }
 	}
-	public static void randomYear(Book b)
-	{
-		int yearRand = (int)(Math.random() * yearCount.size());
-	    int year = yearCount.get(yearRand); 
-	    b.setYearOfPublication(year);
-	    yearCount.remove(yearRand);
-	}
+	
+	//Creates only one instance of each year and assigns it to yearCount
 	public static void setYears()
 	{
-		for (int i = 1980; i < 2020; i++) {
+		for (int i = 1980; i < 2020; i++) 
+		{
 	        yearCount.add(i);
 		}
 	}
+	
+	//Adds a random year of publication to each book in bookList
+	public static void randomYear(Book b)
+	{
+		//Chooses a random year from yearCount
+		int yearRand = (int)(Math.random() * yearCount.size());
+	    int year = yearCount.get(yearRand);
+	    
+	    //Sets year to the book
+	    b.setYearOfPublication(year);
+	    
+	    //Removes the year from the list so no other occurrences of the same year exist
+	    yearCount.remove(yearRand);
+	}
+	
+	//Sorts bookList lowest to highest year of publication dates
 	public static void sortYear()
 	{
-		boolean check = true;
-		while (check)
-		{
-		// https://www.youtube.com/watch?v=3CJq3XV2PBU - modified for lists
+		// https://www.youtube.com/watch?v=3CJq3XV2PBU - modified it for lists
+		/* Uses two for loops in order to compare one value of the list to the one after.
+		 * If the year "i" is greater than the year "j" (the value after the index i), then it will set 
+		 * the position of year "i" to the position of year "j" and set the value of year "j" to year "i".
+		 * It essentially just pushes the values that are higher to the end of the list and pushes back 
+		 * the values that are lower to the front of the list. 
+		 */
 		for (int i = 0; i < bookList.size(); i++)
 		{
 			for(int j = i +1; j < bookList.size(); j++)
@@ -86,10 +111,15 @@ public class Runner {
 				}
 			}
 		}
-		for (Book book: bookList) {
+		
+		//Prints out newly sorted list
+		for (Book book: bookList) 
+		{
 			System.out.println("Title: " + book.getTitle() + "\nSubject: " + book.getSubject()+ "\nYear of Publication: " + book.getYearOfPublication()+ "\nNumber of Pages: " + book.getTotalPages()+ "\nReview Rating: " + book.getReviewRating());
 			System.out.println("");
-			}
+		}
+		
+		//Sends user back to the menu screen
 		Scanner in = new Scanner(System.in);
 		System.out.println("Press enter or any key to go back to menu");
 		String ans = in.nextLine();
@@ -103,13 +133,12 @@ public class Runner {
 			System.out.println("================================================");
 			createMenu();
 		}
-		}
 	}
+	
+	//Sorts bookList lowest to highest total pages
 	public static void sortPages()
 	{
-		boolean check = true;
-		while (check)
-		{
+		//Follows the same sorting method as sortYear() except with pages
 		for (int i = 0; i < bookList.size(); i++)
 		{
 			for(int j = i +1; j < bookList.size(); j++)
@@ -122,9 +151,15 @@ public class Runner {
 				}
 			}
 		}
-		for (Book book: bookList) {
+		
+		//Prints out newly sorted bookList
+		for (Book book: bookList) 
+		{
 			System.out.println("Title: " + book.getTitle() + "\nSubject: " + book.getSubject()+ "\nYear of Publication: " + book.getYearOfPublication()+ "\nNumber of Pages: " + book.getTotalPages()+ "\nReview Rating: " + book.getReviewRating());
-			System.out.println("");}
+			System.out.println("");
+		}
+		
+		//Allows user to return the menu
 		Scanner in = new Scanner(System.in);
 		System.out.println("Press enter or any key to go back to menu");
 		String ans = in.nextLine();
@@ -138,13 +173,12 @@ public class Runner {
 			System.out.println("================================================");
 			createMenu();
 		}
-		}
 	}
+	
+	//Sorts bookList highest to lowest review rating
 	public static void sortReview()
 	{
-		boolean check = true;
-		while (check)
-		{
+		//Follows the same sorting method as sortYear() except it checks if rating "j" is greater than rating "i"
 		for (int i = 0; i < bookList.size(); i++)
 		{
 			for(int j = i +1; j < bookList.size(); j++)
@@ -157,10 +191,15 @@ public class Runner {
 				}
 			}
 		}
-		for (Book book: bookList) {
+		
+		//Prints out newly sorted list
+		for (Book book: bookList) 
+		{
 			System.out.println("Title: " + book.getTitle() + "\nSubject: " + book.getSubject()+ "\nYear of Publication: " + book.getYearOfPublication()+ "\nNumber of Pages: " + book.getTotalPages()+ "\nReview Rating: " + book.getReviewRating());
 			System.out.println("");
-			}
+		}
+		
+		//Allows user to return to menu
 		Scanner in = new Scanner(System.in);
 		System.out.println("Press enter or any key to go back to menu");
 		String ans = in.nextLine();
@@ -174,54 +213,73 @@ public class Runner {
 			System.out.println("================================================");
 			createMenu();
 		}
-		}
 	}
+	
+	//Prints out all books that pertains to one subject
 	public static void sortSubject()
 	{
 		Scanner in = new Scanner (System.in);
+		
+		//A while loop to allow the user to check books of different subjects
 		boolean check = true;
 		while(check)
 		{
 			System.out.println("Which subject are you looking for? Press enter to go back to the menu");
 			String ans = in.nextLine();
+			
+			//Returns the user to the menu if entered is pressed
 			if (ans.equals(""))
 			{
 				System.out.println("================================================");
 				createMenu();
 				break;
 			}
+			
+			//Else it will look for the subject the user typed
 			else
 			{
-			ArrayList<Book> bookSubjectList = new ArrayList<>();
-			for (Book b: bookList)
-			{
-				if(b.getSubject().equals(ans))
+				//An ArrayList of all books found in the subject
+				ArrayList<Book> bookSubjectList = new ArrayList<>();
+				
+				//Goes through each book in bookList to find and add the subject books to bookSubjectList
+				for (Book b: bookList)
 				{
-					bookSubjectList.add(b);
+					if(b.getSubject().equals(ans))
+					{
+						bookSubjectList.add(b);
+					}
 				}
-			}
-			if (bookSubjectList.size() <= 0)
-			{
-				System.out.println("There are no books in your subject.");
-			}
-			else {
-				for (Book book: bookSubjectList) {
-					System.out.println("Title: " + book.getTitle() + "\nSubject: " + book.getSubject()+ "\nYear of Publication: " + book.getYearOfPublication()+ "\nNumber of Pages: " + book.getTotalPages()+ "\nReview Rating: " + book.getReviewRating());
-					System.out.println("");
+				
+				//If there are no books of the subject, then lets user know there are no books in the subject
+				if (bookSubjectList.size() <= 0)
+				{
+					System.out.println("There are no books in your subject.");
 				}
-			}
+				
+				//Else prints out all books with the same subject
+				else 
+				{
+					for (Book book: bookSubjectList) 
+					{
+						System.out.println("Title: " + book.getTitle() + "\nSubject: " + book.getSubject()+ "\nYear of Publication: " + book.getYearOfPublication()+ "\nNumber of Pages: " + book.getTotalPages()+ "\nReview Rating: " + book.getReviewRating());
+						System.out.println("");
+					}
+				}
 			}
 		}
 	}
+	
+	//Displays all books in bookList
 	public static void displayList()
 	{
-		boolean check = true;
-		while (check)
+		//Prints out all books in bookList
+		for (Book book: bookList) 
 		{
-		for (Book book: bookList) {
 			System.out.println("Title: " + book.getTitle() + "\nSubject: " + book.getSubject()+ "\nYear of Publication: " + book.getYearOfPublication()+ "\nNumber of Pages: " + book.getTotalPages()+ "\nReview Rating: " + book.getReviewRating());
 			System.out.println("");
 		}
+		
+		//Allows user to return to the menu
 		Scanner in = new Scanner(System.in);
 		System.out.println("Press enter or any key to go back to menu");
 		String ans = in.nextLine();
@@ -235,17 +293,24 @@ public class Runner {
 			System.out.println("================================================");
 			createMenu();
 		}
-		}
 	}
+	
+	//Finds and provides detail of a book with a specific name
 	public static void searchSpecificName()
 	{
 		Scanner in = new Scanner (System.in);
-		boolean check = true;
+		
+		//Used to check if there are any books in bookList with the same name
 		int alt = 0;
+		
+		//While loop to allow users to look for multiple books
+		boolean check = true;
 		while(check)
 		{
 			alt = 0;
 			System.out.println("What is the name of the book you are looking for? Press enter to go back to the menu");
+			
+			//Allows user to return to the menu
 			String ans = in.nextLine();
 			if (ans.equals(""))
 			{
@@ -253,41 +318,48 @@ public class Runner {
 				createMenu();
 				break;
 			}
+			
+			//*Checks if each book in bookList match the name with the one provided. 
+			//If not, then it will add 1 to alt
 			else
 			{
-			//ArrayList<Book> bookSubjectList = new ArrayList<>();
-			for (Book book: bookList)
-			{
-				if(book.getTitle().equals(ans))
+				for (Book book: bookList)
 				{
-					System.out.println("Title: " + book.getTitle() + "\nSubject: " + book.getSubject()+ "\nYear of Publication: " + book.getYearOfPublication()+ "\nNumber of Pages: " + book.getTotalPages()+ "\nReview Rating: " + book.getReviewRating());
+					if(book.getTitle().equals(ans))
+						System.out.println("Title: " + book.getTitle() + "\nSubject: " + book.getSubject()+ "\nYear of Publication: " + book.getYearOfPublication()+ "\nNumber of Pages: " + book.getTotalPages()+ "\nReview Rating: " + book.getReviewRating());
+					else
+						alt++;
 				}
-				else
-				{
-					alt++;
-				}
-			}
-			if (alt >= 21)
-			{
-				System.out.println("There are no books with the name");
-			}
+				
+				//If the alt is greater than or equal to 21 (meaning that the entirebookList has been iterated through
+				//then let the user that there are no books with the name provided
+				if (alt >= 21)
+					System.out.println("There are no books with the name");
 			}
 		}
 	}
+	
+	//Allows user to add a book to bookList
 	public static void addBook()
 	{
+		//A while loop to allow the user to add more than one book
 		boolean check = true;
 		while(check)
 		{
 			Scanner in = new Scanner (System.in);
 			System.out.println("Do you want to add a book? Type yes to continue, Hit enter to go back to the menu");
 			String ans = in.nextLine();
+			
+			//Sends the user to the menu if entered is hit
 			if (ans.equals(""))
 			{
 				System.out.println("================================================");
 				createMenu();
 				break;
 			}
+			
+			//If the user says yes, then ask for each of the details of the books 
+			//and create a new book object with the given characteristics
 			else if (ans.equals("yes")) {
 				System.out.print("Title of Book: ");
 				String title = in.nextLine();
@@ -302,15 +374,19 @@ public class Runner {
 				Book newBook = new Book(title, subject, yearOfPublication,totalPages,reviewRating);
 				bookList.add(newBook);
 			}
+			
+			//If the user types in any other command, let them know it is invalid
 			else
-			{
 				System.out.println("Invalid Command!");
-			}
 		}
 	}
+	
+	//Creates menu for user to interact with
 	public static void createMenu()
 	{
 		Scanner in = new Scanner(System.in);
+		
+		//Introduction and list of all possible commands
 		System.out.println("Hello! Welcome to Ammaar University's Library Helper \nType a number to access the function \n");
 		System.out.println("1. List of all the books");
 		System.out.println("2. List of all the books sorted from oldest to newest year of publication");
@@ -321,6 +397,8 @@ public class Runner {
 		System.out.println("7. Add a book");
 		System.out.println("8. Exit");
 		String ans = in.nextLine();
+		
+		//Checks the response of the user and executes its respective method
 		if (ans.equals("1")) {
 			System.out.println("================================================");
 			displayList();
@@ -354,8 +432,6 @@ public class Runner {
 			System.exit(0);
 		}
 		else 
-		{
 			System.out.println("Invalid command!");
-		}
 	}
 }
